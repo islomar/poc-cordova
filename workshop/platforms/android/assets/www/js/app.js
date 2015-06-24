3dd5583b96238095566b319a09b2eec4108eb4ad
Isidro Lopez <ilopez@unience.com>
2015-06-24 16:08:14 +0200

    Avoid the 300ms click delay with fastclick.js

--- workshop/platforms/android/assets/www/js/app.js
+++ workshop/platforms/android/assets/www/js/app.js
@@ -12,18 +12,19 @@
     $('.help-btn').on('click', function() {
         alert("Employee Directory v3.4");
     });
-    //document.addEventListener('deviceready', function () {
-    //    if (navigator.notification) { // Override default HTML alert with native dialog
-    //        window.alert = function (message) {
-    //            navigator.notification.alert(
-    //                message,    // message
-    //                null,       // callback
-    //                "Workshop", // title
-    //                'OK'        // buttonName
-    //            );
-    //        };
-    //    }
-    //}, false);
+    document.addEventListener('deviceready', function () {
+        FastClick.attach(document.body);
+        if (navigator.notification) { // Override default HTML alert with native dialog
+            window.alert = function (message) {
+                navigator.notification.alert(
+                    message,    // message
+                    null,       // callback
+                    "Workshop", // title
+                    'OK'        // buttonName
+                );
+            };
+        }
+    }, false);
 
     /* ---------------------------------- Local Functions ---------------------------------- */
     function findByName() {
