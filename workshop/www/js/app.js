498e14403676c96c23d92f9a5af2d0459ae68122
Isidro Lopez <ilopez@unience.com>
2015-06-24 17:25:03 +0200

    Add View Routing and EmployeeView for employee details

--- workshop/www/js/app.js
+++ workshop/www/js/app.js
@@ -4,11 +4,22 @@
     /* ---------------------------------- Local Variables ---------------------------------- */
     HomeView.prototype.template = Handlebars.compile($("#home-tpl").html());
     EmployeeListView.prototype.template = Handlebars.compile($("#employee-list-tpl").html());
+    EmployeeView.prototype.template = Handlebars.compile($("#employee-tpl").html());
 
     var service = new EmployeeService();
     service.initialize().done(function () {
         console.log("Service initialized");
-        $('body').html(new HomeView(service).render().$el);
+        router.addRoute('', function() {
+          $('body').html(new HomeView(service).render().$el);
+        });
+
+        router.addRoute('employees/:id', function(id) {
+          service.findById(parseInt(id)).done(function(employee) {
+              $('body').html(new EmployeeView(employee).render().$el);
+          });
+        });
+
+        router.start();
     });
 
     /* --------------------------------- Event Registration -------------------------------- */
